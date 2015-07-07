efea5d344ad5a9d35913ea00dbc45e5e9a265730
Jason R. McNeil <jason@jasonrm.net>
2015-07-07 14:44:07 -0700

    Fix comparison

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ if [[ -f ./pre_build.sh ]]; then
     ./pre_build.sh || exit 1
 fi
 
-if [[ "${PRE_INSTALL_PACKAGES}" !== "" ]]; then
+if [[ "${PRE_INSTALL_PACKAGES}" != "" ]]; then
 	pacman -Sy ${PRE_INSTALL_PACKAGES}
 fi
 sudo -u build makepkg --noconfirm --syncdeps ${MAKEPKG_OPTIONS} || exit 1
