ffd9b93da9acc27f86218920f021ead5f048d7f0
Jason R. McNeil <jason@jasonrm.net>
2015-07-07 14:43:06 -0700

    Add PRE_INSTALL_PACKAGES support

--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ if [[ -f ./pre_build.sh ]]; then
     ./pre_build.sh || exit 1
 fi
 
+if [[ "${PRE_INSTALL_PACKAGES}" !== "" ]]; then
+	pacman -Sy ${PRE_INSTALL_PACKAGES}
+fi
 sudo -u build makepkg --noconfirm --syncdeps ${MAKEPKG_OPTIONS} || exit 1
 
 if [[ -f ./post_build.sh ]]; then
