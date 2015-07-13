0a32a3feeca10e992e662584894da3298446c75b
Jason R. McNeil <jason@jasonrm.net>
2015-07-13 11:07:47 -0700

    Escape sever vars when appended to pacman.conf

--- nspawn-zfs-build.sh
+++ nspawn-zfs-build.sh
@@ -49,7 +49,7 @@ zfs set mountpoint=${MOUNT_DIR} ${TARGET_FILESYSTEM} || exit 1
 
 cat <<EOT >> ${MOUNT_DIR}/etc/pacman.conf
 [atomica]
-Server = http://aur.atomica.net/$repo/$arch
+Server = http://aur.atomica.net/\$repo/\$arch
 SigLevel = Never
 EOT
 
