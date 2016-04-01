6e2757e9c0b708eb178806119ae585ea9f967af0
Jason R. McNeil <jason@jasonrm.net>
2016-04-01 01:30:14 -0700

    prepare directory

--- build-arch-base.sh
+++ build-arch-base.sh
@@ -3,6 +3,9 @@
 TIMESTAMP=$(date +%s)
 IMAGE_NAME="arch-base"
 
+rm -rf "${IMAGE_NAME}"
+mkdir "${IMAGE_NAME}"
+
 sudo pacstrap -c -d ${IMAGE_NAME} base || exit 1
 
 tar -C ${IMAGE_NAME} -c . | docker import - "${IMAGE_NAME}:${TIMESTAMP}"
