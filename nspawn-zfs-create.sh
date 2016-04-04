10df532c35bc5a740b682cc845057ee0c52390e9
Jason R. McNeil <jason@jasonrm.net>
2016-04-04 07:40:12 +0000

    Drop for now actually

--- nspawn-zfs-create.sh
+++ nspawn-zfs-create.sh
@@ -36,7 +36,7 @@ systemd-nspawn --directory=${MOUNT_DIR} --bind=/var/cache/pacman /bin/sh -c 'pac
 systemd-nspawn --directory=${MOUNT_DIR} --bind=/var/cache/pacman /bin/sh -c 'pacman-key -r 0x4466fcf875b1e1ac && pacman-key --lsign-key 0x4466fcf875b1e1ac' || exit 1
 
 # Copy custom makepkg.conf
-cp ${SCRIPT_DIR}/arch-devel/makepkg.conf ${MOUNT_DIR}/etc/makepkg.conf || exit 1
+# cp ${SCRIPT_DIR}/arch-devel/makepkg.conf ${MOUNT_DIR}/etc/makepkg.conf || exit 1
 
 echo " ==> unmount filesystem: ${FILESYSTEM}"
 umount ${MOUNT_DIR} || exit 1
