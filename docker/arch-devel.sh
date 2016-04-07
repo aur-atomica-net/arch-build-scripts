47878095570dff1fa9ee3a49a21a1cf4871927e7
Jason R. McNeil <jason@jasonrm.net>
2016-04-07 01:02:59 -0700

    Use uid:gid 999:999

--- docker/arch-devel.sh
+++ docker/arch-devel.sh
@@ -6,9 +6,10 @@ set -o pipefail
 pacman -Sy --noconfirm base-devel git
 
 # Setup our build user (makepkg doesn't run as root)
-useradd -g users -G wheel -s /bin/bash build
+groupadd -r -g 999 build
+useradd -g build -G wheel -u 999 -s /bin/bash build
 mkdir -p /home/build
 mkdir -p /home/build/.gnupg
 echo 'keyserver hkp://pool.sks-keyservers.net' > /home/build/.gnupg/gpg.conf
 echo 'keyserver-options auto-key-retrieve' >> /home/build/.gnupg/gpg.conf
-chown -R build:users /home/build
+chown -R build:build /home/build
