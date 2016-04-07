0c875d31bccd43d10b96df3b21f440f6f104eab2
Jason R. McNeil <jason@jasonrm.net>
2016-04-07 00:28:38 -0700

    setup build user in arch-devel

--- docker/arch-devel.sh
+++ docker/arch-devel.sh
@@ -4,3 +4,11 @@ set -x
 set -o pipefail
 
 pacman -Sy --noconfirm base-devel git
+
+# Setup our build user (makepkg doesn't run as root)
+useradd -g users -G wheel -s /bin/bash build
+mkdir -p /home/build
+mkdir -p /home/build/.gnupg
+echo 'keyserver hkp://pool.sks-keyservers.net' > /home/build/.gnupg/gpg.conf
+echo 'keyserver-options auto-key-retrieve' >> /home/build/.gnupg/gpg.conf
+chown -R build:users /home/build
